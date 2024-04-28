312b670de4ec7bf91ee496163dbc892cc9b2bb6b
MattJWS <majustus@senators.ws.edu>
2024-04-28 15:08:40 -0400

    Added CardViewModel model

---
+++ MagicApp/Models/CardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicApp.Models
+{
+    public class CardViewModel
+    {
+        public List<Card> Cards { get; set; } = null!;
+        public List<Color> Colors { get; set; } = null!;
+        public List<CardType> CardTypes { get; set; } = null!;
+        public List<SuperType> SuperTypes { get; set; } = null!;    
+    } // end class
+} // end namespace
